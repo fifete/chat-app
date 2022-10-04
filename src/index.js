a7b99bb4dd7f660d02e7c7c9b0cf99c38d64014e
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-10-04 18:21:33 -0500

    funciones de bd en index, funcionando

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const db = require('./queries');
+// eslint-disable-next-line import/extensions
+const client = require('./connection.js');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -18,10 +19,84 @@ app.listen(port, () => {
 
 app.use(bodyParser.json());
 
+client.connect();
+
+function getUsers() {
+  return client
+    .query('SELECT * FROM users')
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 app.get('/users', (req, res) => {
-  const getU = Promise.all(db.getUsers());
-  console.log(getU);
-  db.getUsers().then((response) => {
+  getUsers().then((response) => {
     res.send(response);
   });
 });
+
+function addUser(nameUser, email, password, status) {
+  client
+    .query(
+      `INSERT INTO public.users(name_user, email, password, status)
+          VALUES ( '${nameUser}', '${email}', '${password}', ${status})`
+    )
+    .then((response) => {
+      console.log(response.rows);
+      client.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+}
+
+/* addUser('Daniela', 'dan@kity.com', '123456', true);
+  addUser('Gaby', 'gaby@kity.com', '123456', true); */
+/* addUser('Pao', 'pao@kity.com', '123456', false); */
+
+function getUserState(status) {
+  client
+    .query(`SELECT * FROM users WHERE status=${status}`)
+    .then((response) => {
+      console.log(response.rows);
+      client.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+}
+/* getUserState(true); */
+
+function updateUserState(id, status) {
+  client
+    .query(`UPDATE public.users SET status=${status} where id_user=${id}`)
+    .then((response) => {
+      console.log(response.rows);
+      client.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+}
+
+/* updateUserState(3, true); */
+
+function deleteUser(id) {
+  client
+    .query(`DELETE FROM users WHERE id_user= ${id};`)
+    .then((response) => {
+      console.log(response.rows);
+      client.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      client.end();
+    });
+}
+/* deleteUser(3); */
