ed8a0fea18e6f27d3a3c3641c046237c3b7df04a
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-10-04 10:17:33 -0500

    socket

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
-const express = require('express');
+/* const express = require('express'); */
 
 const bodyParser = require('body-parser');
 
-const app = express();
+/* const app = express(); */
 // eslint-disable-next-line import/extensions
 const client = require('./connection.js');
 
+/* const port = process.env.PORT || 3300; */
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
 const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
 
-app.listen(port, () => {
+// base de datos
+
+/* app.get('/', (req, res) => {
+  res.send('Hello World!');
+}); */
+
+/* app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
+}); */
 
 app.use(bodyParser.json());
 
