4a47768022dd8132aaa29982feb4c2923098291a
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-10-08 12:41:25 -0500

    envío de token a login y envío de errores a register

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+/* eslint-disable import/newline-after-import */
+/* eslint-disable prettier/prettier */
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
+
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 3100;
 const port2 = process.env.PORT || 3300;
 const io = require('socket.io')(http, {
@@ -11,26 +16,11 @@ const io = require('socket.io')(http, {
   },
 });
 
+const client = require('./connection.js');
+const db = require('./queries');
 app.use(cors());
 
 // eslint-disable-next-line import/extensions
-const client = require('./connection.js');
-const db = require('./queries');
-
-
-// const express = require('express');
-
-// const app = express();
-
-// const port = process.env.PORT || 3300;
-// const path = require('path');
-
-// import queries
-
-/* app.get('/', (req, res) => {
-  res.json({ info: 'Node.js, Express, and Postgres API' });
-});
- */
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
@@ -52,9 +42,9 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log(msg);
-    io.emit('chat message', msg.text);
+  socket.on('chat message', (msgInfo) => {
+    console.log(msgInfo);
+    io.emit('chat message', msgInfo);
   });
 });
 
@@ -62,25 +52,39 @@ http.listen(port2, () => {
   console.log(`listening on: ${port2}`);
 });
 
+app.post("/login", (req , res) => {
+  const infoUserLogin = req.body;
+  jwt.sign({infoUserLogin}, 'secretkey', {expiresIn: '24h'}, (err, token) => {
+      res.send({
+          token
+      });
+  });
 
-/* app.get('/userState', (req, res) => {
-  bd.getUserState(false).then((response) => {
-    res.send(response);
+});
+
+// Authorization: Bearer <token>
+function verifyToken(req, res, next){
+  const bearerHeader =  req.headers.authorization;
+
+  if(typeof bearerHeader !== 'undefined'){
+       const bearerToken = bearerHeader.split(" ")[1];
+       req.token  = bearerToken;
+       next();
+  }else{
+      res.sendStatus(403);
+  }
+}
+
+app.post("/createCanal", verifyToken, (req , res) => {
+
+  jwt.verify(req.token, 'secretkey', (error, authData) => {
+      if(error){
+          res.sendStatus(403);
+      }else{
+          res.json({
+                  mensaje: "Canal fue creado",
+                  authData
+              });
+      }
   });
 });
-
-app.post('/addUser', (req, res) => {
-  console.log('asks');
-  bd.addUser('kitty', 'pam@kity.com', '123456', true)
-  res.send('adding kitty');
-
-}); */
-
-/* app.put('/updateState', (req, res) => {
-  bd.updateUserState(2, false)
-});
-
-app.delete('/deleteUser', (req, res) => {
-  bd.deleteUser(4)
-}); */
-
--- src/queries.js
+++ src/queries.js
@@ -10,13 +10,14 @@ const getUsers = (request, response) => {
 };
 
 function addUser(req, res) {
-  const { nameUser, email, password, status } = req.body;
+  const { nameUser, email, password } = req.body;
   client.query(
-    `INSERT INTO public.users(username, state, email)
-      VALUES ($1, $2, $3, $4);`,
-    [nameUser, email, password, status],
+    `INSERT INTO public.users(user_name, email, password)
+      VALUES ($1, $2, $3);`,
+    [nameUser, email, password],
     (error, results) => {
       if (error) {
+        res.status(400).send({error: error.detail});
         throw error;
       }
       res.status(201).send(`User added with ID: ${results.rows}`);
@@ -27,6 +28,17 @@ function addUser(req, res) {
 /* addUser('Daniela', 'dan@kity.com', '123456', true);
     addUser('Gaby', 'gaby@kity.com', '123456', true); */
 /* addUser('Pao', 'pao@kity.com', '123456', false); */
+
+function getUserState(status) {
+  client
+    .query(`SELECT * FROM users WHERE status=${status}`)
+    .then((response) => {
+      console.log(response.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 function getUserState(status) {
   client
