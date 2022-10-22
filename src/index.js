db0f0d117f0c42e5d096a24d2d7f886225da13fe
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-10-22 10:50:11 -0500

    cerrar sesión

--- src/index.js
+++ src/index.js
@@ -47,7 +47,15 @@ app.get('/', (req, res) => {
 app.post('/addUser', db.addUser);
 
 let users = [];
-
+const channels = {
+  current: '1'
+}
+/* 
+{
+  1: [users]
+  cid2: [users del canal 2]
+}
+ */
 io.on('connection', (socket) => {
   socket.on('chat message', (msgInfo) => {
     const { room } = msgInfo;
@@ -81,16 +89,77 @@ io.on('connection', (socket) => {
     const presentUser = users.find(user => user.socketID === socket.id);
     console.log('🔥: A user disconnected', presentUser, socket.id);
     db.updateUserState(presentUser, 'false');
-    users = users.filter(user =>user.socketID !== socket.id);   
+/*     users = users.filter(user =>user.socketID !== socket.id);  */  
     io.emit("newUserResponse", presentUser.email)
     socket.disconnect()
   });
 
-  socket.on('joinChannel', (channel) => {
-    console.log(channel);
-    socket.join(channel.name_channel);
+  socket.on('logOut', () => {
+    const presentUser = users.find(user => user.socketID === socket.id);
+    console.log('🔥: A user disconnected', presentUser, socket.id);
+    db.updateUserState(presentUser, 'false');
+    io.emit("newUserResponse", presentUser.email)
+    socket.disconnect()
   });
 
+  /* 
+{
+  current: '1'
+  1: [users]
+  cid2: [users del canal 2]
+}
+'3': [
+    {
+      email: 'user1@gmail.com',
+      uid: 1,
+      user_name: 'user1',
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGF0YSI6eyJlbWFpbCI6InVzZXIxQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoidXNlcjEifSwiaWF0IjoxNjY2Mjc2MDM3LCJleHAiOjE2NjYzNjI0Mzd9.g_kkn-_5n-WKFOV8_tZGjYw1tzi9bWyjHO3vGScHxYI'
+    },
+    {
+      email: 'user2@gmail.com',
+      uid: 2,
+      user_name: 'user2',
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGF0YSI6eyJlbWFpbCI6InVzZXIyQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoidXNlcjIifSwiaWF0IjoxNjY2Mjc2NzQxLCJleHAiOjE2NjYzNjMxNDF9.lCN1HsAwJKpgPUJIwPtx0JV7UdKjW3L1DI6yRQ8vUmA'
+    },
+    {
+      email: 'user2@gmail.com',
+      uid: 2,
+      user_name: 'user2',
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGF0YSI6eyJlbWFpbCI6InVzZXIyQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoidXNlcjIifSwiaWF0IjoxNjY2Mjc2NzQxLCJleHAiOjE2NjYzNjMxNDF9.lCN1HsAwJKpgPUJIwPtx0JV7UdKjW3L1DI6yRQ8vUmA'
+    }
+  ]
+ */
+  socket.on('joinChannel', (userInChannelInfo) => {
+    const { channelInfo, user} = userInChannelInfo
+    console.log(channelInfo);
+    socket.join(channelInfo.name_channel);
+    const {cid} = channelInfo
+    channels.current = cid
+    if(channels[cid]) {
+      console.log('prep', channels[cid]);
+      channels[cid] = [...channels[cid]].concat(user)
+      channels[cid] = channels[cid].filter((channel, index, channelArr) =>
+      index === channelArr.findIndex((c) => (
+        c.email === channel.email
+      )));
+      // channels[cid].forEach(channel => {
+      //   console.log('prep', channel.email, user.email);
+      //   if(channel.email !== user.email){
+      //     console.log('post', channels[cid]);
+
+      //   }
+      // })
+      console.log('post', channels[cid]);
+    } 
+    else channels[cid] = [].concat(user)
+    // socket.broadcast.to(channelInfo.name_channel).emit('joinChannel', channels[cid]);
+    io.to(channelInfo.name_channel).emit('usersInRoom', {
+      current: cid,
+      [cid]: channels[cid]
+    });
+    // io.emit("usersInRoom", channels);
+    console.log('my', channels);
+  });
 });
 
 http.listen(port2, () => {
@@ -113,5 +182,4 @@ function verifyToken(req, res, next){
       res.sendStatus(403).send();
   }
 }
-// app.post('/addChannel', db.addChannel);
 app.post("/addChannel", verifyToken, db.addChannel);
